c89f0891f264ee28b4281eb6aae6b5dbc508b594
ALina <laymalay@gmail.com>
2017-11-04 15:23:58 +0300

    simple realisation of tcp-server

---
+++ tcp-server/server.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef SERVER_H
+#define SERVER_H
+#include <QTcpServer>
+#include <QTime>
+#include <QTcpSocket>
+#include <QWidget>
+#include <QMessageBox>
+#include <QTextEdit>
+#include <QLayout>
+#include <QLabel>
+class QTcpServer;
+class QTextEdit;
+class QTcpSocket;
+
+namespace Ui {
+class Server;
+}
+
+class Server : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Server(int nPort,QWidget *parent = 0);
+    ~Server();
+public slots:
+    virtual void slotNewConnection();
+    void slotReadClient();
+
+private:
+    QTcpServer* m_ptcpServer;
+    QTextEdit* m_ptxt;
+    quint16 m_nNextBlockSize;
+    Ui::Server *ui;
+private:
+    void sendToClient(QTcpSocket* pSocket, const QString& str);
+
+};
+
+#endif // SERVER_H
